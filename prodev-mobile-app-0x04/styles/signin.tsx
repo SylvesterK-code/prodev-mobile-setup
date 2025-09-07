1c26bb58192db7787b57bfdd925a1f4622a96fcc
sylvesterK-code <sylvesterkeystarling@gmail.com>
2025-09-07 06:49:46 +0000

    0. Navigation using Stack Navigation Patterns

--- prodev-mobile-app-0x04/styles/signin.tsx
+++ prodev-mobile-app-0x04/styles/signin.tsx
@@ -3,120 +3,113 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
-    position: 'relative'
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
   },
   navGroup: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-    marginTop: 11
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 22,
   },
   largeText: {
-    fontSize: 40,
     fontWeight: "700",
-  },
-    mediumText: {
-    fontSize: 20,
-    color: "#fd003fff",
-    marginBottom: 10,
-    fontWeight: "800",
-    textAlign: "right"
+    fontSize: 39,
+    marginTop: 20,
   },
   smallText: {
+    fontWeight: "400",
     fontSize: 12,
-    color: '#7E7B7B'
+    color: "#7E7B7B",
+    marginTop: 5,
+  },
+  formGroup: {
+    marginTop: 44,
+    rowGap: 10,
   },
   placeholderText: {
     fontSize: 18,
-    color: '#7E7B7B',
-    marginBottom: 7
+    fontWeight: "400",
+    color: "#7B7B7B",
+    marginBottom: 7,
   },
   inputField: {
-    borderWidth: 1,
-    borderRadius: 10,
+    borderWidth: 2,
     height: 50,
-    borderColor: '#7E7B7B',
-    paddingHorizontal: 10
+    borderRadius: 10,
+    borderColor: "#E9E9E9",
+    paddingHorizontal: 10,
   },
   passwordGroup: {
-    flexDirection: 'row',
-    borderWidth: 1,
-    alignItems: 'center',
+    borderWidth: 2,
     height: 50,
     borderRadius: 10,
+    borderColor: "#E9E9E9",
     paddingHorizontal: 10,
-    borderColor: '#7e7b7b'
-  },
-  formGroup: {
-    marginTop: 44
-  },
-  forgotPasswordText: {
-    fontSize: 17,
-    marginTop: 9,
-    textAlign: 'right',
-    color: '#34967C'
+    flexDirection: "row",
+    alignItems: "center",
   },
   button: {
-    backgroundColor: '#2B876E',
+    backgroundColor: "#34967C",
     height: 53,
     borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 25,
-    justifyContent: 'center',
-    alignItems: 'center'
   },
   buttonText: {
+    color: "white",
     fontSize: 17,
-    color: 'white'
-  },
-  socialMediaButton: {
-    borderWidth: 1,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10
-  },
-  socialMediaButtonText: {
-    fontSize: 18,
-    color: '#0D0D0D',
-    fontWeight: "400"
-  },
-  socialMediaButtonGroup: {
-    gap: 15,
-    marginTop: 25
+    fontWeight: "300",
   },
   dividerGroup: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 10,
-    marginTop: 29
+    flexDirection: "row",
+    alignItems: "center",
+    columnGap: 10,
+    marginBottom: 25,
+    marginTop: 29,
   },
   divider: {
-    borderWidth: 0.5,
-    borderColor: "#c2c2c2",
-    flex: 1
+    borderWidth: 1,
+    flex: 1,
+    borderColor: "#e6e6e6",
   },
   dividerText: {
     fontSize: 17,
+    fontWeight: "500",
     color: "#C2C2C2",
   },
+  socialMediaButtonGroup: {
+    rowGap: 15,
+    marginTop: 15,
+  },
+  socialMediaButton: {
+    height: 53,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    columnGap: 10,
+    borderWidth: 1,
+  },
+  socialMediaButtonText: {
+    fontSize: 18,
+    fontWeight: "400",
+  },
   subTextGroup: {
-    flexDirection: 'row',
-    position: 'absolute',
-    bottom: 33,
-    left: 77,
-    right: 76
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 25,
   },
   subText: {
     fontSize: 18,
-    color: '#b5b5b5'
+    fontWeight: "400",
   },
   subTextJoin: {
     fontSize: 18,
-    color: '#FFA800',
-    fontWeight: "600"
-  }
+    fontWeight: "600",
+    color: "#FFA800",
+    marginLeft: 5,
+  },
 });
 
-// export {styles};
